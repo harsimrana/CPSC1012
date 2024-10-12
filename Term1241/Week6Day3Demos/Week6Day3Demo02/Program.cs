e1720b33cfcf19de9b159af577d4c254c42a39d9
harsimrana <harsimrana@nait.ca>
2024-10-11 18:07:20 -0600

    Week6Day3Demo1and2 Done
    Before Break part

---
+++ Term1241/Week6Day3Demos/Week6Day3Demo02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Week6Day3Demo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Week 6=> Day 3: Demo 02
+            int menuChoice = 0;
+            char userChoice='N';
+            bool isValid = true;
+
+            do // Outer loop to control whether user would like to perform this again.
+            {
+                Console.WriteLine("Press 1: For addition ");
+                Console.WriteLine("Press 2: For Subtraction ");
+                Console.WriteLine("Press 3: For Multiplication ");
+                Console.WriteLine("Press 4: For Division ");
+
+                Console.Write("Enter your choice: ");
+                menuChoice = int.Parse(Console.ReadLine());
+
+                isValid = true;
+                // Write your logic for Calculator
+
+                while (isValid) // Nested loop to control user behaviour for input
+                {
+                    try
+                    {
+                        Console.Write("Do you like to perform it again? [Y, N]: ");
+                        userChoice = char.Parse(Console.ReadLine());
+                        if(userChoice=='Y' || userChoice == 'y' || userChoice == 'n' || userChoice == 'N')
+                        {
+                            isValid = false;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        isValid = true;
+                        Console.WriteLine("Don't try to be a smart user!! Enter a right response!!");
+                    }
+                }
+            } while (userChoice == 'Y' || userChoice == 'y');  // Semi-colon is required with do while
+
+            Console.WriteLine("Good bye! Thank you for using my calculator");
+
+        }
+    }
+}
---
+++ Term1241/Week6Day3Demos/Week6Day3Demos/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Week6Day3Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Formatting strings
+            int i = 150;
+            double f = 1234.56789;
+
+            // Adding Colors: Bonus
+            Console.ForegroundColor = ConsoleColor.Blue; // Text color
+
+            //Change back ground color
+
+            Console.BackgroundColor = ConsoleColor.White; // Background of Text
+
+            // Title for my program
+            Console.WriteLine("Formattig of strings");
+            Console.WriteLine("======================");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red; // Text color
+
+            //Change back ground color
+
+            Console.BackgroundColor = ConsoleColor.Black; // Background of Text
+
+            Console.WriteLine("i: {0:0000} f: {1:0.00}", i, f);
+
+            // Make it even for fancy
+            Console.WriteLine("i: {0:#,##0} f: {1:##,##0.00}", i, f);
+
+            // Reset color settings
+
+            Console.ResetColor(); // Reset color back to default colors
+
+            // Print in columns- Right aligned 
+            Console.WriteLine("i: {0,10:0} f: {1,15:0.00}", i, f);
+            Console.WriteLine("i: {0,10:0} f: {1,15:0.00}", 0, 0);
+
+            // Print in columns- Left aligned minus sign will cause it to be left aligned
+            Console.WriteLine("i: {0,-10:0} f: {1,-15:0.00}", i, f);
+            Console.WriteLine("i: {0,-10:0} f: {1,-15:0.00}", 0, 0);
+
+            
+        }
+    }
+}
