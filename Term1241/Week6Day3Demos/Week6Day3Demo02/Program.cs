765cb4b6ca3c1b42dd06ac24468ade9b4ebd122e
harsimrana <harsimrana@nait.ca>
2024-10-11 18:20:49 -0600

    Final Commit of the Day

--- Term1241/Week6Day3Demos/Week6Day3Demo02/Program.cs
+++ Term1241/Week6Day3Demos/Week6Day3Demo02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Week6Day3Demo02
+﻿using System;
+
+namespace Week6Day3Demo02
 {
     internal class Program
     {
@@ -16,12 +18,51 @@
                 Console.WriteLine("Press 3: For Multiplication ");
                 Console.WriteLine("Press 4: For Division ");
 
-                Console.Write("Enter your choice: ");
-                menuChoice = int.Parse(Console.ReadLine());
+                isValid = true;
+
+                while (isValid)  // Logic to control first input
+                {
+                    try
+                    {
+                        Console.Write("Enter your choice: ");
+                        menuChoice = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Don't try to be a smart user!! Enter a right response!!");
+                    }
+                    // Write your logic for Calculator
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            //write your logic for addition
+                            Console.WriteLine($"You have pressed {menuChoice}");
+                            isValid = false;
+                            break;
+                        case 2:
+                            //write your logic for sub
+                            Console.WriteLine($"You have pressed {menuChoice}");
+                            isValid = false;
+                            break;
+                        case 3:
+                            //write your logic for Mul
+                            Console.WriteLine($"You have pressed {menuChoice}");
+                            isValid = false;
+                            break;
+                        case 4:
+                            //write your logic for divisio
+                            Console.WriteLine($"You have pressed {menuChoice}");
+                            isValid = false;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong Choice:Enter a number between 1 and 4");
+                            isValid = true;
+                            break;
+                    }
+                }
 
                 isValid = true;
-                // Write your logic for Calculator
-
+                
                 while (isValid) // Nested loop to control user behaviour for input
                 {
                     try
@@ -39,6 +80,7 @@
                         Console.WriteLine("Don't try to be a smart user!! Enter a right response!!");
                     }
                 }
+
             } while (userChoice == 'Y' || userChoice == 'y');  // Semi-colon is required with do while
 
             Console.WriteLine("Good bye! Thank you for using my calculator");
