68d5b94aaae281775c6b7446d29a2e4f0818e40e
harsimrana <harsimrana@nait.ca>
2024-10-16 17:53:33 -0600

    Demo01 Half Done

---
+++ Term1241/Week07DemoFiles/Week07DemoFiles/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Week07DemoFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Week 7: Day 2
+            // using simple variables
+            /*
+            int a = 4, b = 5, c = 6, d=7, e=4;
+            int sum;
+
+            sum = a + b + c + d + e;
+
+            Console.WriteLine($"Sum of numbers: {sum}");
+            */
+            // Create and array
+            // Syntax
+            // Data type [] name = new Datatype [N];
+
+            int[] scoreList = new int[5];
+            int i; // Loop controller
+
+            // Assign values to array elements
+            for (i = 0; i <= 4; ++i)
+            {
+                Console.Write("Enter value: ");
+
+                scoreList[i] = int.Parse(Console.ReadLine());
+            }
+            /*
+            scoreList[1] = 5;
+            scoreList[2] = 6;
+            scoreList[3] = 7;
+            scoreList[4] = 9;
+            */
+
+            // Access any element in an array
+            // arrayName[index]
+            for (i = 0; i <= 4; ++i)
+            {
+                Console.WriteLine($"{i+1} Element {scoreList[i]}");
+            }
+            /*
+            Console.WriteLine($"2nd Element {scoreList[1]}");
+            Console.WriteLine($"3rd Element {scoreList[2]}");
+            Console.WriteLine($"4th Element {scoreList[3]}");
+            Console.WriteLine($"5th Element {scoreList[4]}");
+            */
+
+
+        }
+    }
+}
