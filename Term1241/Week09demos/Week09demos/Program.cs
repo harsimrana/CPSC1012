5cbe34512433295abb254e7cb69dd21798df1130
harsimrana <harsimrana@nait.ca>
2024-10-28 17:58:07 -0600

    Demo 01 First Commit

---
+++ Term1241/Week09demos/Week09demos/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Week09demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call your method
+            //First Call to my method
+            DoSomething("Something", 5);  // arguments
+            //Second call with different values
+            DoSomething("Harsimranjot", 10);
+
+            int n = 4;
+
+            int r = CalculateSqaure(n);
+
+            Console.WriteLine($" Square of {n} = {r}");
+
+            r = CalculateSqaure(5);
+
+            Console.WriteLine($"Square of number is {r}");
+
+        }
+        // Method declaration for method DoSomething
+        // Naming: 
+        // 1. Pascal naming notation
+        // 2. Start with verb
+        //                              parameters
+        public static void DoSomething(string message, int a)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(a);
+        }
+
+        public static int CalculateSqaure(int a)
+        {
+            int result;
+
+            result = a * a;
+
+            //Console.WriteLine($" Sqaure of {a}= {result}");
+            // Return a value back calling location
+
+            return result; //Only one value can be returned
+        }
+    }
+}
