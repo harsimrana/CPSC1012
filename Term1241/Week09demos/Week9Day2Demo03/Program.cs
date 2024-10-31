1597f2fffae1d0aba4a7d81aecaeedcc45ec8aff
harsimrana <harsimrana@nait.ca>
2024-10-30 18:19:48 -0600

    Third Commit

---
+++ Term1241/Week09demos/Week9Day2Demo03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week9Day2Demo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call by value vs Call by reference
+            int a = 5, b = 7;
+            
+            Console.WriteLine($"Before Calling method: Value of a = {a} and b = {b}");
+
+            ChangeValues(ref a, ref b);
+
+            Console.WriteLine($"After Calling method: Value of a = {a} and b = {b}");
+
+        }
+
+        static void ChangeValues(ref int a, ref int b)
+        {
+            Console.WriteLine($" Inside Method: Value of a = {a} and b = {b}");
+            a = 10;
+            b = 15;
+            Console.WriteLine($" Inside Method After change: Value of a = {a} and b = {b}");
+        }
+    }
+}
