e94ee9d6bafb1c2f63065c0662e1809546821206
harsimrana <harsimrana@nait.ca>
2024-11-06 16:58:04 -0700

    Co

--- Term1241/Week10Day1Demos/Week10Day1Demos/obj/Debug/net6.0/Week10Day1Demos.AssemblyInfo.cs
+++ Term1241/Week10Day1Demos/Week10Day1Demos/obj/Debug/net6.0/Week10Day1Demos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week10Day1Demos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0b3d4c9ecd2a336a667c048a25cb595f4f9deb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36fa67d2bcf522d705578cef547337730f57f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week10Day1Demos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week10Day1Demos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
