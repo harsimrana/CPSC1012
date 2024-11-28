ead9d9c173bd733adef480ef2ca2d0b4a70c5046
harsimrana <harsimrana@nait.ca>
2024-11-27 17:43:18 -0700

    Final commit

---
+++ Term1241/Week13Day02Demos/Week13Day02Demos/Dog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Day02Demos
+{
+    internal class Dog
+    {
+        
+        //some variable - State
+        private string _name;
+        private string _breed;
+        private int _age;
+        private string _color;
+
+        // Methods- Behaviour of object
+        // Constructor
+        public Dog() // Default constructor is always there
+        { // We are overriding it
+            _name = "";
+            _breed = "";
+            _age = 0;
+            _color = "white";
+        }
+        //Constructor Overloading 
+        public Dog(string name, string breed, int age, string color) // Default constructor is always there
+        { // We are overriding it
+            // validate data and assign those values to states
+            _name = name;
+            _breed = breed;
+            _age = age;
+            _color = color;
+
+        }
+        // Some methods
+        public void PrintDetails()
+        {
+            Console.WriteLine("Dog Details ");
+            Console.WriteLine("==================");
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine($"Breed: {_breed}");
+            Console.WriteLine($"Age: {_age}");
+            Console.WriteLine($"Color: {_color}");
+        }
+        public void Bark()
+        {
+            Console.WriteLine($"{_name} is barking now");
+        }
+        // Eat()
+        // Walk()
+
+    }
+}
---
+++ Term1241/Week13Day02Demos/Week13Day02Demos/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Week13Day02Demos
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of Dog class
+            Dog lily = new Dog();  // Calling Defautl constructor 
+
+            // Print dog details
+            lily.PrintDetails();
+
+            // Let's use another constuctor 
+            Dog myDog = new Dog("Tommy", "German Shepherd", 7, "Brown");
+            //Calling Greedy Constructor
+
+            myDog.PrintDetails();
+
+            //Console.WriteLine("Simran");
+           
+        }
+    }
+}
